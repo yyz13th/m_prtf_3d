9dc25e435f19fd48cc9b195ff125a174a7c2159c
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-06 17:32:54 +0300

    animation logics

--- src/models/plane.model.jsx
+++ src/models/plane.model.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import { useEffect, useRef } from 'react'
 
-import planeScene from '../assets/3d/spaceship.glb'
-import { useGLTF } from '@react-three/drei'
+import planeScene from '../assets/3d/plane.glb'
+import { useAnimations, useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+const Plane = ({isRotating, ...props}) => {
+    const ref = useRef();
     const { scene, animations } = useGLTF(planeScene)
+    const { actions } = useAnimations(animations, ref)
+
+    useEffect(() => {
+        if(isRotating) {
+            actions['Take 001'].play();
+        } else {
+            actions['Take 001'].stop();
+        }
+    }, [isRotating, actions])
   return (
-    <mesh>
-        <primitive object={scene} scale={[0.1, 0.1, 0.1]}/>
+    <mesh {...props} ref={ref}>
+        <primitive object={scene} /> 
+        {/* <primitive object={scene} scale={[0.1, 0.1, 0.1]} /> */}
     </mesh>
   )
 }
