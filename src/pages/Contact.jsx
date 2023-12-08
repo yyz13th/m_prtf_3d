09f25161acb7158b3300f39fa6e75c2ae2c6cbc7
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-08 16:12:18 +0300

    contacts page + forms + emailjs integration

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,110 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
+  const formRef = useRef(null)
+  const [form, setForm] = useState({name: '', email: '', message: ''});
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  }; //alllows to update inputs
+
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    //emailjs site service + template + KEY stored as env local vars
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+        from_name: form.name,
+        to_name: 'Egor',
+        from_email: form.email,
+        to_email: 'yz13th@gmail.com',
+        message: form.message
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+    ).then(()=> {
+      setIsLoading(false);
+      alert('Message sent!');
+      setForm({name: '', email: '', message: ''});
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      alert('Something went wrong!')
+    })
+  };
+
+
   return (
-    <div>Contact</div>
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Get in touch</h1>
+
+        <form 
+          className='w-full flex flex-col gap-7 mt-14'
+          onSubmit={handleSubmit}>
+          <label className='text-black-500 font-semibold'>
+            Name
+            <input
+            type='text'
+            name='name'
+            className='input'
+            placeholder='Name'
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+
+          <label className='text-black-500 font-semibold'>
+            Email
+            <input
+            type='email'
+            name='email'
+            className='input'
+            placeholder='Name@example.com'
+            required
+            value={form.email}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+
+          <label className='text-black-500 font-semibold'>
+            Message
+            <textarea
+            name='message'
+            rows={4}
+            className='textarea'
+            placeholder='Let me know how I can help you'
+            required
+            value={form.message}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            />
+          </label>
+
+          <button 
+            type='submit' 
+            className='btn'
+            disabled={isLoading}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section> 
   )
 }
 
