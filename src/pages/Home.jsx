1b4c4a8919d8f344ab89ef4d5cb91e5dc9238b8d
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-11 13:57:05 +0300

    object positioning

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -72,8 +72,8 @@ const Home = () => {
               setCurrentStage = {setCurrentStage}
             />
             <Plane 
-            planeScale = {planeScale}
-            planePosition = {planePosition}
+            scale = {planeScale}
+            position = {planePosition}
             isRotating = {isRotating}
             rotation = {[0, 20, 0]}/>
           </Suspense>
