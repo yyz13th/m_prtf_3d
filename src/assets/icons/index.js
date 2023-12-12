e8e11f4ac555b3f704475c42a322b51ceb01eec0
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-12 16:12:31 +0300

    addition icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -26,6 +26,7 @@ import arrow from './arrow.svg'
 import contact from './contact.svg'
 import soundon from './soundon.png'
 import soundoff from './soundoff.png'
+import play from './play.svg'
 
 export {
     css,
@@ -55,5 +56,6 @@ export {
     arrow,
     contact,
     soundon,
-    soundoff
+    soundoff,
+    play
 }
