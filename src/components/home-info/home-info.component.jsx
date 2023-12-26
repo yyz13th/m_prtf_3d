e0d177fb37b373ad6b758d987bf751d51e73e982
yyz13th <136233684+yyz13th@users.noreply.github.com>
2023-12-26 16:22:03 +0300

    added fading settings

--- src/components/home-info/home-info.component.jsx
+++ src/components/home-info/home-info.component.jsx
@@ -19,11 +19,11 @@ const InfoBox = ({ text, link, btnText}) => (
 
 const renderInfoText = {
     1: (
-        <div className='lg:w-1/2 gap-7 text-center font-josefin opacity-100 transition-opacity duration-500' style={{ position: 'absolute', top: '55%', left: '25%' }}>
+        <div className='lg:w-1/2 gap-7 text-center font-josefin ex_fade-in-op' style={{ position: 'absolute', top: '55%', left: '25%' }}>
             <h2 className='text-3xl pb-5 text-center font-nanum  text-white' style={{fontStyle: 'italic'}}>Hi</h2>
         <h1 className='text-7xl sm:text-5xl text-white font-nanum' style={{letterSpacing: '0.2em'}}> I am Egor </h1>
             <hr className='pb-12 mt-12 lg:w-1/2 mx-auto'/>
-            <h2 className='text-5xl font-light text-white'> Web-developer/system engineer </h2>
+            <h2 className='text-5xl font-light text-white shadow-gray'> Web-developer/system engineer </h2>
         
         </div>
     ),
