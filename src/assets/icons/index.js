f55ea7d7572a7b838651807c7e3140f8c1a82433
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-20 15:34:00 +0300

    add imports

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,7 @@ import css from './css.svg'
 import express from './express.svg'
 import git from './git.svg'
 import github from './github.svg'
+import githubw from './githubw.svg'
 import html from './html.svg'
 import javascript from './javascript.svg'
 import mongodb from './mongodb.svg'
@@ -33,6 +34,7 @@ export {
     express,
     git,
     github,
+    githubw,
     html,
     javascript,
     mongodb,
