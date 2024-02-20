94bb152f86355f158ea0e48412b7fe50325d4423
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-20 15:34:22 +0300

    style fixes

--- src/components/home-info/home-info.component.jsx
+++ src/components/home-info/home-info.component.jsx
@@ -1,55 +1,55 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { arrow } from '../../assets/icons'
 import useFadeIn from '../../hooks/useFadeIn'
 
 // const { opacity } = useFadeIn(800);
 
 
-const InfoBox = ({ text, link, btnText}) => (
-    
-    <div className='main-box w-[60%]'>
-           <p className='sm:text-2xl text-center font-josefin font-light' style={{ transition: 'opacity 0.5s ease-in-out'}}>{text}</p>
-        <Link to={link} className='neo-brutalism-white neo-btn'>
-            {btnText}
-            <img src={arrow} alt='arrow' className='w-4 h-4 object-contain'/>
-        </Link>
+const InfoBox = ({ text, link, btnText }) => (
+    <div style={{ position: 'absolute', top: '50%', left: '20%' }}>
+        <div className='main-box w-[60%]'>
+            <p className='sm:text-2xl text-center font-josefin font-light' style={{ transition: 'opacity 0.5s ease-in-out' }}>{text}</p>
+            <Link to={link} className='neo-brutalism-white neo-btn'>
+                {btnText}
+                <img src={arrow} alt='arrow' className='w-4 h-4 object-contain' />
+            </Link>
+        </div>
     </div>
 )
 
 const renderInfoText = {
     1: (
         <div className='lg:w-1/2 gap-7 text-center font-josefin ex_fade-in-op' style={{ position: 'absolute', top: '55%', left: '25%' }}>
-            <h2 className='text-3xl pb-5 text-center font-nanum  text-white' style={{fontStyle: 'italic'}}>Hi</h2>
-        <h1 className='text-7xl sm:text-5xl text-white font-nanum' style={{letterSpacing: '0.2em'}}> I am Egor </h1>
-            <hr className='pb-12 mt-12 lg:w-1/2 mx-auto'/>
-            <h2 className='text-5xl font-light text-white shadow-gray'> Web-developer/system engineer </h2>
-        
+            <h2 className='text-3xl pb-5 text-center font-nanum  text-white' style={{ fontStyle: 'italic' }}>Hi</h2>
+            <h1 className='text-7xl sm:text-5xl text-white font-nanum' style={{ letterSpacing: '0.2em' }}> I am Egor </h1>
+            <hr className='pb-12 mt-12 lg:w-1/2 mx-auto' />
+            <h2 className='text-5xl font-light text-white shadow-gray'> Web-developer/System engineer </h2>
         </div>
     ),
     2: (
-        <InfoBox 
-        text=' Worked with many companies and picked up many skills along the way'
-        link='/about'
-        btnText='Learn more'/> 
+        <InfoBox
+            text=' Worked with many companies and picked up many skills along the way'
+            link='/about'
+            btnText='Learn more' />
     ),
     3: (
-        <InfoBox 
-        text=' Led multiple projects to success over the years. Curious about the impact?'
-        link='/projects'
-        btnText='Visit my portfolio'/> 
+        <InfoBox
+            text=' Led multiple projects to success over the years. Curious about the impact?'
+            link='/projects'
+            btnText='Visit my portfolio' />
     ),
     4: (
-        <InfoBox 
-        text=' Need a project done or looking for a dev? Im just a few keystrokes away'
-        link='/contacts'
-        btnText='Lets talk'/> 
+        <InfoBox
+            text=' Need a project done or looking for a dev? Im just a few keystrokes away'
+            link='/contacts'
+            btnText='Lets talk' />
     )
 }
 
 
-const HomeInfo = ({currentStage}) => {
-  return renderInfoText[currentStage] || null;
+const HomeInfo = ({ currentStage }) => {
+    return renderInfoText[currentStage] || null;
 }
 
 export default HomeInfo
