091c933c20906b98055cb1a01af50fd3f2804158
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-20 15:29:34 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/navbar.component'
 import { Home, About, Projects, Contact } from './pages';
 
 const App = () => {
     return (
-    <main className='bg-slate-300/20 h-full'>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-                <Route path='/projects' element={<Projects/>}/>
-                <Route path='/contacts' element={<Contact/>}/>
-            </Routes>
-        </Router>
-    </main>
+        <main className='bg-slate-300/20 h-full'>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/projects' element={<Projects />} />
+                    <Route path='/contacts' element={<Contact />} />
+                </Routes>
+            </Router>
+        </main>
     )
 }
 
