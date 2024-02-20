04d3a630423d9f5dece31508d0177029e74c6dc9
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-20 15:31:06 +0300

    divided canvases+loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,24 @@ import CanvasTwo from '../components/canvases/CanvasTwo.component'
 import CanvasOne from '../components/canvases/CanvasOne.component'
 import CanvasThree from '../components/canvases/CanvasThree.component';
 const Home = () => {
-  
+
   const { progress } = useProgress();
   return (
- <section className='w-full h-screen relative' style={{ backgroundColor: 'black' }}>
-      <div className='ex_fade-in-op'>{progress  < 100 ? (
+    <section className='w-full h-screen relative' style={{ backgroundColor: 'black' }}>
+      <div className='ex_fade-in-op'>{progress < 100 ? (
         <div className="absolute flex flex-col items-center justify-center text-4xl gap-7 font-josefin font-regular text-yellow-600 h-screen top-1/2 left-1/2 z-10 transform -translate-x-1/2 -translate-y-1/2">
-          <PacmanLoader color="#c2b72e" loading={true} size={20} /> 
+          <PacmanLoader color="#c2b72e" loading={true} size={20} />
           <br />
-          <p className='shadow-yellow'>{` ${progress }%`}</p>
+          <p className='shadow-yellow'>{` ${progress}%`}</p>
         </div>
       ) : (
         <>
-            {/* <CanvasOne />
+          {/* <CanvasOne />
             <CanvasTwo /> */}
-            <CanvasThree /> 
-            </>
-      )} </div>
+          <CanvasThree />
+        </>
+      )}
+      </div>
     </section>
   )
 }
